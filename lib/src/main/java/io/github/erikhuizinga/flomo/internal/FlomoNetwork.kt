d4ecd10bc77b9f0739fd5892fbf90b4937972879
Erik Huizinga <erik.huizinga@nedap.com>
2019-07-05 22:46:12 +0200

    ✨ Add SDK 28 FlomoNetwork implementation
    Based on Network

--- lib/src/main/java/io/github/erikhuizinga/flomo/internal/FlomoNetwork.kt
+++ lib/src/main/java/io/github/erikhuizinga/flomo/internal/FlomoNetwork.kt
@@ -1,10 +1,19 @@
 package io.github.erikhuizinga.flomo.internal
 
+import android.net.Network
 import android.net.NetworkInfo
 
 internal sealed class FlomoNetwork(val isConnected: Boolean) {
 	abstract override fun equals(other: Any?): Boolean
 	abstract override fun hashCode(): Int
+}
+
+internal class Flomo28Network(
+	private val network: Network?,
+	isConnected: Boolean
+) : FlomoNetwork(isConnected) {
+	override fun equals(other: Any?) = other is Flomo28Network && other.network == network
+	override fun hashCode() = network?.hashCode() ?: 0
 }
 
 internal class FlomoCompatNetwork(
