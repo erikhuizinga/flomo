7717450eeb8af87f8a518ffc3e3b8d1fd3e8d9ca
Erik Huizinga <erik.huizinga@nedap.com>
2019-07-05 22:06:21 +0200

    ✨ Add pre-SDK 28 FlomoNetworkStream implementation

---
+++ lib/src/main/java/io/github/erikhuizinga/flomo/internal/FlomoCompatNetworkStream.kt
@@ -0,0 +1,35 @@
+package io.github.erikhuizinga.flomo.internal
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.ProducerScope
+import kotlinx.coroutines.launch
+
+@ExperimentalCoroutinesApi
+internal class FlomoCompatNetworkStream(
+	override val producerScope: ProducerScope<FlomoCompatNetwork>
+) : BroadcastReceiver(),
+	FlomoNetworkStream<FlomoCompatNetwork> {
+	override fun subscribe(context: Context) {
+		context.registerReceiver(
+			this,
+			IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+		)
+	}
+
+	override fun onReceive(context: Context?, intent: Intent?) {
+		intent
+			?.getParcelableExtra<NetworkInfo>(ConnectivityManager.EXTRA_NETWORK_INFO)
+			?.let { networkInfo ->
+				producerScope.apply { launch { send(FlomoCompatNetwork(networkInfo)) } }
+			}
+	}
+
+	override fun unsubscribe(context: Context) =
+		context.unregisterReceiver(this)
+}
