1a94b90220869269acdf1313d8798334c902eb49
Erik Huizinga <erik.huizinga@nedap.com>
2019-08-14 09:07:43 +0200

    ✨ Add buildSrc

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,20 @@
+// Top level plugin versions
+const val ANDROID_GRADLE = "3.4.2"
+const val KOTLIN = "1.3.41"
+const val DETEKT = "1.0.0-RC16"
+
+// Module plugin versions
+const val BINTRAY_RELEASE = "0.9.1"
+
+// Android config
+const val COMPILE_SDK_VERSION = 28
+const val MIN_SDK_VERSION = 14
+const val TARGET_SDK_VERSION = 28
+
+// Module dependency versions
+const val KOTLINX_COROUTINES = "1.3.0-M2"
+const val ANDROIDX_APPCOMPAT = "1.0.2"
+const val ANDROIDX_CORE_KTX = "1.0.2"
+const val JUNIT = "4.12"
+const val MOCKK = "1.9.3"
+const val LEAK_CANARY = "2.0-alpha-3"
