c2d93c2f4b801f5bec6fcbf79872cac0cd1d8d13
Erik Huizinga <erik.huizinga@nedap.com>
2019-08-17 10:07:16 +0200

    🎨 Simplify flow implementation using map

--- lib/src/main/java/io/github/erikhuizinga/flomo/Flomo.kt
+++ lib/src/main/java/io/github/erikhuizinga/flomo/Flomo.kt
@@ -4,23 +4,24 @@ import android.content.Context
 import io.github.erikhuizinga.flomo.internal.FlomoNetwork
 import io.github.erikhuizinga.flomo.internal.flomoNetworkFlow
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 
 /**
  * Flow network connectivity status ([Boolean]) in a non-blocking way.
  */
 @ExperimentalCoroutinesApi
-val Context.isNetworkConnectedFlow
-    get() = flow {
+val Context.isNetworkConnectedFlow: Flow<Boolean>
+    get() {
         val connectedNetworks = mutableSetOf<FlomoNetwork>()
-        flomoNetworkFlow.collect { flomoNetwork ->
-            if (flomoNetwork.isConnected) {
-                connectedNetworks += flomoNetwork
-            } else {
-                connectedNetworks -= flomoNetwork
-            }
-            emit(connectedNetworks.any())
-        }
-    }.distinctUntilChanged()
+        return flomoNetworkFlow
+            .map { flomoNetwork ->
+                if (flomoNetwork.isConnected) {
+                    connectedNetworks += flomoNetwork
+                } else {
+                    connectedNetworks -= flomoNetwork
+                }
+                connectedNetworks.any()
+            }.distinctUntilChanged()
+    }
